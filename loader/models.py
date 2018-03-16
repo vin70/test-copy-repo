51805683f9fdc24250bd768496157c71d624aacf
Kirill Elizarov <kelizarov@localhost.localdomain>
2018-03-16 12:46:57 +0700

    test

--- loader/models.py
+++ loader/models.py
@@ -1,3 +1,4 @@
+import cProfile
 import cv2
 import boto3
 import asyncio
@@ -8,13 +9,15 @@ class Loader(object):
         self.cam = cv2.VideoCapture(0)
         self.client = boto3.client('rekognition','eu-west-1')
         self.img = None
+        self.pr = cProfile.Profile()
 
-    async def task(self):
+    async def take_image(self):
         _, self.img = self.cam.read()
-        asyncio.ensure_future(self.task())
+        asyncio.ensure_future(self.take_image())
 
     async def send_to_server(self):
         if self.img is not None:
+            self.pr.enable()
             img = cv2.imencode('.jpg', self.img)[1].tostring()
             response = self.client.detect_labels(Image={'Bytes': img})
                 
@@ -23,6 +26,8 @@ class Loader(object):
                 print (label['Name'] + ' : ' + str(label['Confidence']))
 
             print('Done...')
+            self.pr.disable()
+            # self.pr.print_stats(sort='time')
         await asyncio.sleep(1)
         asyncio.ensure_future(self.send_to_server())
     
--- run.py
+++ run.py
@@ -1,12 +1,14 @@
 import asyncio
+import cProfile
 from loader.models import Loader
 
 if __name__ == "__main__":
     print("Starting client...")
+
     client = Loader()
 
     ioloop = asyncio.get_event_loop()
-    asyncio.ensure_future(client.task())
+    asyncio.ensure_future(client.take_image())
     asyncio.ensure_future(client.send_to_server())
     ioloop.run_forever()
     ioloop.close()
