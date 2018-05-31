dc4ed2167e76cf1c30a5b7bd81950c94a022ad49
Kirill Elizarov <kirillthetee@gmail.com>
2018-05-31 10:34:36 +0700

    added argv launch parameter

--- run.py
+++ run.py
@@ -1,20 +1,22 @@
 import asyncio
+import sys
 from loader.models import Loader
 
 if __name__ == "__main__":
-    print("Starting client...")
 
     client = Loader()
-    # client.process_images()
-    imgs = client.get_local_imgs()
-    for img in imgs:
-        labels = client.get_labels(img)
-        client.print_labels(labels)
-        labels_list = client.filter_labels(labels)
-        print(labels_list)
-
-    # ioloop = asyncio.get_event_loop()
-    # asyncio.ensure_future(client.take_image())
-    # asyncio.ensure_future(client.send_to_server())
-    # ioloop.run_forever()
-    # ioloop.close()
+    if 'camera' in sys.argv:
+        print("Starting camera process...")
+        ioloop = asyncio.get_event_loop()
+        asyncio.ensure_future(client.take_image())
+        asyncio.ensure_future(client.send_to_server())
+        ioloop.run_forever()
+        ioloop.close()
+    else:
+        print("Starting image process...")
+        imgs = client.get_local_imgs()
+        for img in imgs:
+            labels = client.get_labels(img)
+            client.print_labels(labels)
+            filtered_lables = client.filter_labels(labels)
+            print(filtered_lables)
